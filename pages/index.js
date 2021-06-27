0cccd0495b361d23569c42ce28f19d1819a0cb51
usmanfarooqi88 <71569086+usmanfarooqi88@users.noreply.github.com>
2021-06-27 19:02:13 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hey.!
+          Hey..!
         </h1>
 
         <p className={styles.description}>
